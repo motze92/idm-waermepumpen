f2ba8261be942da75f252d4a9efa2e8dfbf13df7
Moritz Mair <mail@moritzmair.info>
2022-10-03 19:47:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+<p align="center">
+ <h2 align="center">IDM Waermepumpen</h2>
+ <p align="center">Communicate to your IDM Wärmepumpen heatpump</p>
+  <p align="center">
+    <a href="https://github.com/motze92/idm-waermepumpen/issues">
+      <img alt="Issues" src="https://img.shields.io/github/issues/motze92/idm-waermepumpen?style=flat&color=336791" />
+    </a>
+    <a href="https://github.com/motze92/idm-waermepumpen/pulls">
+      <img alt="GitHub pull requests" src="https://img.shields.io/github/issues-pr/motze92/idm-waermepumpen?style=flat&color=336791" />
+    </a>
+     <a href="https://github.com/motze92/idm-waermepumpen">
+      <img alt="GitHub Downloads" src="https://img.shields.io/npm/dw/idm-waermepumpen?style=flat&color=336791" />
+    </a>
+    <a href="https://github.com/motze92/idm-waermepumpen">
+      <img alt="GitHub Total Downloads" src="https://img.shields.io/npm/dt/idm-waermepumpen?color=336791&label=Total%20downloads" />
+    </a>
+ <a href="https://github.com/motze92/idm-waermepumpen">
+      <img alt="GitHub release" src="https://img.shields.io/github/release/motze92/idm-waermepumpen.svg?style=flat&color=336791" />
+    </a>
+    <br />
+    <br />
+  <a href="https://github.com/motze92/idm-waermepumpen/issues/new/choose">Report Bug</a>
+  <a href="https://github.com/motze92/idm-waermepumpen/issues/new/choose">Request Feature</a>
+  </p>
+
+
+
+# Getting started
+
+## Installation
+
+
+## 📝 License
+
+Copyright © 2022 [Moritz Mair](https://github.com/motze92).<br />
+This project is [MIT](LICENSE) licensed.
---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import uuid from './helpers/uuid';
+import ModbusRTU from 'modbus-serial';
+
+export default class IDM {
+    client = new ModbusRTU();
+    private IP: string;
+    private PORT: number;
+    private ID: number;
+
+    constructor(IP: string, PORT?: number, ID?: number) {
+        this.IP = IP;
+        this.PORT = PORT ? PORT : 502;
+        this.ID = ID ? ID : 1;
+    }
+
+    async connect() {
+        await this.client.connectTCP(this.IP, { port: this.PORT });
+        this.client.setID(this.ID);
+    }
+
+    async readFloatRegister(register: number) {
+        const res = await this.client.readHoldingRegisters(register, 2)
+        const buf = Buffer.allocUnsafe(4)
+        buf.write(res.buffer.toString('hex').substring(4, 8) + res.buffer.toString('hex').substring(0, 4), 0, 4, 'hex')
+        return buf.readFloatBE(0);
+    }
+
+    async writeFloatRegister(register: number, value: number) {
+        const buf = Buffer.allocUnsafe(4)
+        buf.writeFloatBE(value)
+        const buf2 = Buffer.allocUnsafe(4)
+        buf2.write(buf.toString('hex').substring(4, 8) + buf.toString('hex').substring(0, 4), 0, 4, 'hex')
+        const res = await this.client.writeRegisters(register, buf2)
+        return res;
+    }
+
+    async setPvSurplus(value: number) {
+        return await this.writeFloatRegister(74, value);
+    }
+
+    async setHeatingElementPower(value: number) {
+        return await this.writeFloatRegister(76, value);
+    }
+
+    async setPvPower(value: number) {
+        return await this.writeFloatRegister(78, value);
+    }
+
+    async setHomeConsumption(value: number) {
+        return await this.writeFloatRegister(82, value);
+    }
+
+    async setBatteryCharge(value: number) {
+        return await this.writeFloatRegister(84, value);
+    }
+
+    async setBatteryLevel(value: number) {
+        return await this.writeFloatRegister(86, value);
+    }
+
+};
